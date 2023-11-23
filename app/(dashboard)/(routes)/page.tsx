a7053705ba2821365bf2fa9bff24db2664522ad3
crohnarodev <viniciosalfa@hotmail.com>
2023-11-23 18:04:28 -0300

    feat: :sparkles: adding redirect embed button to redirect to login page

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,14 +1,10 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-medium text-sky-700'>Hello</p>
-      <Button>
-        Click me
-      </Button>
+      <p>Hello</p>
+      <UserButton afterSignOutUrl="/"/>
     </div>
-    
   )
 }
