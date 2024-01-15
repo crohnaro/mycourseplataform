810ef40d0fede6e2a646a3a288e31d15c7827598
crohnarodev <viniciosalfa@hotmail.com>
2024-01-15 14:48:29 -0300

    feat: :sparkles: using toast provider on api function

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -19,6 +19,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -42,7 +43,7 @@ const CreatePage = () => {
       const response = await axios.post("/api/course", values);
       router.push(`/teacher/courses${response.data.id}`);
     } catch {
-      console.log("Something went wrong");
+      toast.error("Something went wrong")
     }
   };
   return (
