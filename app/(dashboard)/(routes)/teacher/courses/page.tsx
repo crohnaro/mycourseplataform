c1102f01613271f4345aa840af0f6f0b5c873f09
crohnaro <vinicioscararine95@gmail.com>
2024-06-04 19:44:02 -0300

    fix: :bug: href on link button

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const CoursesPage = () => {
-    return ( 
-        <div className="p-6">
-            <Button>
-                New Course
-            </Button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
+};
+
 export default CoursesPage;
