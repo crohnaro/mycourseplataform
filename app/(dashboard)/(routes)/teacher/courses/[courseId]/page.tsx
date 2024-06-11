83efc9d7b6b3930de8ffa156ad7bfdcaedce91ac
crohnaro <vinicioscararine95@gmail.com>
2024-06-11 18:45:13 -0300

    feat: :sparkles: adding reusable icon to the project

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -40,6 +42,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             {" "}
             Complete all fields {completionText}
           </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
         </div>
       </div>
     </div>
