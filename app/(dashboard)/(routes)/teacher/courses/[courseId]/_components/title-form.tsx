b2b2aa445d962b0d1da7585d4561731f439c2b3e
crohnaro <vinicioscararine95@gmail.com>
2024-06-28 15:52:36 -0300

    feat: :sparkles: adding edit title btn

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,51 +1,67 @@
-"use client"
+"use client";
 
-import * as z from "zod"
-import axios from "axios"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage
-} from "@/components/ui/form"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
 
-interface TitleFormProps{
-    initialData: {
-        title: string
-    }
-    courseId: string
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
 }
 
 const formSchema = z.object({
-    title: z.string().min(1,{
-        message: "Titulo é obrigatório"
-    })
-})
+  title: z.string().min(1, {
+    message: "Titulo é obrigatório",
+  }),
+});
 
-export const TitleForm = ({
-    initialData,
-    courseId
-}: TitleFormProps) =>{
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: initialData,
-    })
+export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
 
-    const {isSubmitting, isValid} = form.formState
+  const [isEditting, setIsEditting] = useState(false);
+  const toggleEdit = () => setIsEditting((current) => !current)
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) =>{
-        console.log(values)
-    }
-    return (
-        <div>
-            Title Form
-        </div>
-    )
-}
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <div className="mt-6 bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Titulo do Curso
+        <Button onClick={toggleEdit}>
+          {isEditting && <>Cancel</>}
+          {!isEditting && (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Editar Titulo
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditting && (
+        <p className="text-sm mt-2">{initialData.title}</p>
+      )}
+    </div>
+  );
+};
