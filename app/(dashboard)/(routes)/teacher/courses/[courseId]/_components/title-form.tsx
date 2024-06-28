06a5085ca0a443ffd3be05be97c359fc64d7c8f5
crohnaro <vinicioscararine95@gmail.com>
2024-06-28 15:22:19 -0300

    feat: :sparkles: adding formschema, props and handlers to the form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,6 +1,48 @@
-"use cliente"
+"use client"
 
-export const TitleForm = () =>{
+import * as z from "zod"
+import axios from "axios"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+interface TitleFormProps{
+    initialData: {
+        title: string
+    }
+    courseId: string
+}
+
+const formSchema = z.object({
+    title: z.string().min(1,{
+        message: "Titulo é obrigatório"
+    })
+})
+
+export const TitleForm = ({
+    initialData,
+    courseId
+}: TitleFormProps) =>{
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    })
+
+    const {isSubmitting, isValid} = form.formState
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) =>{
+        console.log(values)
+    }
     return (
         <div>
             Title Form
